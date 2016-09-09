ee9770c943c9c6abf3b5c60d3dda24fbd9e15d55
James Freeman <freeman.james.h@gmail.com>
2016-09-09 14:19:35 -0600

    bcrypt sign-up complete

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const config = require('../knexfile.js')[process.env.DATABASE_ENV]
 const knex = require('knex')(config)
+const bcrypt = require('bcrypt')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,17 +12,32 @@ router.get('/', function(req, res, next) {
 router.post('/signup', function(req, res, next) {
     res.cookie('isLoggedIn', true)
     let username = req.body.username;
-    let hashed_pw = req.body.password
-    knex('users').insert({username : username, hashed_pw : hashed_pw})
-    .then(function(){
-      res.redirect('/')
-    })
+    knex('users').whereRaw('lower(username) = ?', req.body.username.toLowerCase())
+        .count()
+        .first() //not sure if/why this necessary but keeping it for now
+        .then(function(result) {
+            if (result.count === "0") {
+                let hashed_pw = bcrypt.hashSync(req.body.password, 8)
+                console.log('user: ', username, hashed_pw)
+                knex('users').insert({
+                    username: username,
+                    hashed_pw: hashed_pw
+                }).returning('*').then(function(results){
+                    console.log(results)
+                }).then(function() {
+                    res.redirect('/profile')
+                })
+            } else {
+                console.log('already there');
+                res.redirect('/')
+            }
+        })
 })
 
-router.post('/logout', function(req, res, next){
-  console.log('Posted to logout')
-  res.clearCookie('isLoggedIn')
-  res.redirect('/')
+router.post('/logout', function(req, res, next) {
+    console.log('Posted to logout')
+    res.clearCookie('isLoggedIn')
+    res.redirect('/')
 })
 
 router.get('/profile', function(req, res, next) {
